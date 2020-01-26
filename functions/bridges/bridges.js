9d449918c8d61b7112a8c2e22de4d641cd52286d
Tatty Bogle <tattybogle@gmail.com>
2020-01-26 20:23:33 +0000

    more tweaks

--- functions/bridges/bridges.js
+++ functions/bridges/bridges.js
@@ -2,29 +2,38 @@ const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event, context) => {
 
-    const url = 'https://trafficscotland.org/bridgerestrictions/index.aspx';
+  const url = 'https://trafficscotland.org/bridgerestrictions/index.aspx';
 
-    const browser = await chromium.puppeteer.launch({
-        executablePath: await chromium.executablePath,
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        headless: chromium.headless,
-    });
-    
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1000, height: 600 });
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless,
+  });
 
-    await page.goto(url);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1000, height: 600 });
 
-    const element = await page.$('.main');
-    const ss = await element.screenshot();
-    const b64 = ss.toString('base64');
+  await page.goto(url);
 
-    await browser.close();
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ "src": `data:image/png;base64,${b64}` })
-    };
+  const element = await page.$('.main');
+
+  await page.evaluate(() => {
+    let h = document.querySelector('.main')
+    let d = h.querySelector('h2');
+    let t = h.querySelector('table');
+    h.innerHTML = `<h3>${d.innerHTML}</h3>`;
+    h.appendChild(t);
+  });
+
+  const ss = await element.screenshot();
+  const b64 = ss.toString('base64');
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ "src": `data:image/png;base64,${b64}` })
+  };
 
 }
